e8f4b3bbbbe75342a65d25a4c1561b14eada342c
Maria Eduarda de Araujo Fernandes <84139806+d-araujof@users.noreply.github.com>
2024-05-09 19:59:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,?!;:()[]{}-_/\\|*&^%$#@=+<>\"\'';
-let shuffledAlphabet = alphabet.split('').sort(() => Math.random() - 0.5).join('');
-let reverseAlphabet = alphabet.split('').reverse().join('');
+    const key = {
+        'a': 'x', 'b': 'q', 'c': 'w', 'd': 'e', 'e': 'r', 'f': 't', 'g': 'y', 'h': 'u', 'i': 'i', 'j': 'o', 'k': 'p', 'l': 'a', 'm': 's',
+        'n': 'd', 'o': 'f', 'p': 'g', 'q': 'h', 'r': 'j', 's': 'k', 't': 'l', 'u': 'z', 'v': 'x', 'w': 'c', 'x': 'v', 'y': 'b', 'z': 'n',
+        'A': 'X', 'B': 'Q', 'C': 'W', 'D': 'E', 'E': 'R', 'F': 'T', 'G': 'Y', 'H': 'U', 'I': 'I', 'J': 'O', 'K': 'P', 'L': 'A', 'M': 'S',
+        'N': 'D', 'O': 'F', 'P': 'G', 'Q': 'H', 'R': 'J', 'S': 'K', 'T': 'L', 'U': 'Z', 'V': 'X', 'W': 'C', 'X': 'V', 'Y': 'B', 'Z': 'N',
+        ' ': ' ', '.': '.', ',': ',', '?': '?', '!': '!', ';': ';', ':': ':', '(': '(', ')': ')', '[': '[', ']': ']', '{': '{', '}': '}',
+        '-': '-', '_': '_', '/': '/', '\\': '\\', '|': '|', '*': '*', '&': '&', '^': '^', '%': '%', '$': '$', '#': '#', '@': '@', '=': '=',
+        '+': '+', '<': '<', '>': '>', '"': '"', '\'': '\''
+    };
 
-function encryptText(text) {
-    let result = '';
-    for (let i = 0; i < text.length; i++) {
-        let char = text[i];
-        let index = alphabet.indexOf(char);
-        if (index !== -1) {
-            result += shuffledAlphabet[index];
-        } else {
-            result += char;
-        }
+    function encryptText(text) {
+        return text.split('').map(char => key[char] || char).join('');
     }
-    return result;
-}
 
-function decryptText(text) {
-    let result = '';
-    for (let i = 0; i < text.length; i++) {
-        let char = text[i];
-        let index = shuffledAlphabet.indexOf(char);
-        if (index !== -1) {
-            result += alphabet[index];
-        } else {
-            result += char;
+    function decryptText(text) {
+        const reverseKey = {};
+        for (let k in key) {
+            reverseKey[key[k]] = k;
         }
+        return text.split('').map(char => reverseKey[char] || char).join('');
     }
-    return result;
-}
 
-const encryptButton = document.getElementById('encryptButton');
-encryptButton.addEventListener('click', () => {
-    const inputText = document.getElementById('inputText').value;
-    const outputText = encryptText(inputText);
-    document.getElementById('outputText').textContent = outputText;
-});
+    const encryptButton = document.getElementById('encryptButton');
+    encryptButton.addEventListener('click', () => {
+        const inputText = document.getElementById('inputText').value;
+        const outputText = encryptText(inputText);
+        document.getElementById('outputText').textContent = outputText;
+    });
 
-const decryptButton = document.getElementById('decryptButton');
-decryptButton.addEventListener('click', () => {
-    const inputText = document.getElementById('inputText').value;
-    const outputText = decryptText(inputText);
-    document.getElementById('outputText').textContent = outputText;
-});
+    const decryptButton = document.getElementById('decryptButton');
+    decryptButton.addEventListener('click', () => {
+        const inputText = document.getElementById('inputText').value;
+        const outputText = decryptText(inputText);
+        document.getElementById('outputText').textContent = outputText;
+    });
